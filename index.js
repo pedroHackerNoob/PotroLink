c2b49386ef2a52278a0bf2a3f25dd173eb9f4793
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-05-28 19:42:37 -0700

    ESM added

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-
+import express from 'express'
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('hola mundo desde express')
+    res.send('hola mundo express')
 } )
+ const port = process.env.PORT|| 4000
 
-
-app.listen(4000,()=>{
-    console.log('Servidor funcionando en localhost:4000')
+app.listen(port,()=>{
+    console.log('Servidor funcionando en localhost: ',port)
 })
