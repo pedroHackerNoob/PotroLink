484f77b0223929019c9b49de0d9a2c565d34bd15
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-05-29 00:00:26 -0700

    api configured

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hola chacales')
-})
+router.post('/auth/register', (req, res) => {
+    res.send('hi from register'+req.body);
+    console.log('hi from register', req.body);
+});
 
-router.get('/blog', (req, res) => {
-    res.send('Hola bloggeros ')
-})
-router.get('/nosotros', (req, res) => {
-    res.send('Hola nosotres')
-})
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import router from './router'
 
 const app = express()
+
+app.use(express.json())
 app.use('/',router)
 
 export default app
