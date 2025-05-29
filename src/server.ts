944b4787f41f6958e342792c9358ed357ba5819e
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-05-29 15:28:29 -0700

    db conection addded

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import router from './router'
+import 'dotenv/config'
+
+import {connectDB} from "./config/db";
 
 const app = express()
-
+connectDB()
 app.use(express.json())
 app.use('/',router)
 
