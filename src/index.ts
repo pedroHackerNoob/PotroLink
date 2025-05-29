edbc8bcd8878cd5ae4998dbb753ad8182bb2ebe6
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-05-28 23:00:18 -0700

    config:     "@types/express": "^5.0.2",

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('hola mundo express')
+    res.send('hola mundo Express / TypeScript')
 } )
  const port = process.env.PORT|| 4000
 
