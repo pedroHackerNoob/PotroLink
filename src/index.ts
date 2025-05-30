16a177258551c8b17c44d38614b6a243b89b4ff9
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-05-30 16:01:35 -0700

    model defined

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import server from "./server";
 import colors from "colors";
-
 const port = process.env.PORT|| 4000
 
 server.listen(port,()=>{
---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+export interface IUser {
+    name: string,
+    email: string,
+    password: string,
+}
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 20,
+    }
+})
+const User = mongoose.model<IUser>('User', userSchema)
+export default User
