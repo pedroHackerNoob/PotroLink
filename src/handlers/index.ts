df0bc135c9f3e88ffd2630410465c357fd1e044b
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-06-03 15:54:01 -0700

    log user info deleted

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -25,7 +25,7 @@ export const createAccount =  async (req : Request, res : Response) => {
         user.password = await hashPassword(password)
         user.handle = handle;
         await user.save();
-        res.status(201).send(`User created successfully:\n`+user)
+        res.status(201).send(`User created successfully:`)
         return console.log(`User created successfully:`)
     }
 }
