3e8e2f8fdcb81b0c8646f020e7524669be4d34f0
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-06-05 12:00:12 -0700

    colors added

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,19 +1,18 @@
 import { CorsOptions } from 'cors'
+import colors from "colors";
 // cors
 export const corsConfig : CorsOptions= {
     origin: function (origin, callback) {
         const whitelist = [process.env.FRONTEND_URL]
-
         if(process.argv[2] ==='--api') {
             whitelist.push(undefined)
-            console.log(whitelist)
         }
         if (whitelist.includes(origin)) {
             callback(null, true)
-            console.log('Connection allowed by CORS')
+            console.log(colors.bgGreen('Connection allowed by CORS'))
         }else {
-            callback(new Error('Not connection allowed by CORS'))
-            console.log('not allowed')
+            callback(new Error('Not connection allowed by CORS'));
+            console.log(colors.bgRed('not allowed'))
         }
     }
 }
